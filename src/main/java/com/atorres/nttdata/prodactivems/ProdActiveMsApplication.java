f1ed4dc65b82150c9f00eebeefd74fce112ecac3
AlejandroTorresUNMSM <alephdmc2575@gmail.com>
2023-07-22 12:28:11 -0500

    dev: commit init

---
+++ src/main/java/com/atorres/nttdata/prodactivems/ProdActiveMsApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.prodactivems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdActiveMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdActiveMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/prodactivems/ProdActiveMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.prodactivems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdActiveMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
